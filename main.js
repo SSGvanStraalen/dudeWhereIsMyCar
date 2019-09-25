49d1eee223a5b3607d23b5aa6d33980732be20e7
Sven van Straalen <ssgvanstraalen@gmail.com>
2019-09-25 16:49:57 +0200

    added styling and workbox

--- main.js
+++ main.js
@@ -3,26 +3,36 @@ let savedLocationElement = document.querySelector('#savedLocation');
 let currentLocationIsKnown = false;
 let currentLocation;
 
+// // Check that service workers are supported
+// if ('serviceWorker' in navigator) {
+//   // Use the window load event to keep the page load performant
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js');
+//   });
+// }
+
 const setCurrentLocationInit = function setTheCurrentLocationForTheFirstTime() {
     return new Promise(resolve =>{
         navigator.geolocation.getCurrentPosition(location =>{
+            console.log('kom ik hier?')
             currentLocation = location;
             resolve(true);
-        }, undefined, {
+        }, e =>{console.log(e)}, {
             enableHighAccuracy: true
         })
     })
 }
 
 const init = async function initializeTheApp(){
+    console.log('init ')
     await setCurrentLocationInit();
     showCurrentPosition(currentLocation);
-    showSavedPosition(JSON.parse(localStorage.savedGPSPosition));
+    if(localStorage.savedGPSPosition){
+        showSavedPosition(JSON.parse(localStorage.savedGPSPosition));
+    }
     navigator.geolocation.watchPosition(updateCurrentLocation, undefined, {
         enableHighAccuracy: true
     });
-    console.log('appready');
-
 }
 
 const saveLocation = async function saveCurrentGPSLocationToLocalStorage(){
@@ -41,7 +51,6 @@ const updateCurrentLocation = async function updateCurrentLocationTroughWatch(po
     showCurrentPosition(position);
     currentLocation = position;
 }
-
 
 function showCurrentPosition(position) {
     locationElement.innerHTML = "Latitude: " + position.coords.latitude + 
