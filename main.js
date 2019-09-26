783cad249260157d228dea0d4573bdf7aef9bfd6
Sven van Straalen <ssgvanstraalen@gmail.com>
2019-09-26 18:58:51 +0200

    added name

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let currentLocation;
 if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('service-worker.js');
   });
 }
 
