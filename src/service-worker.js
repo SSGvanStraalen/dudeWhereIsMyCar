adb38c0c5621c0fedecbfed67cad010898d36342
Sven van Straalen <ssgvanstraalen@gmail.com>
2019-10-03 11:48:48 +0200

    added useless buildstepp lol

--- src/service-worker.js
+++ src/service-worker.js
@@ -14,11 +14,8 @@ if (workbox) {
   );
 
   workbox.routing.registerRoute(
-    // Cache CSS files.
     /\.css$/,
-    // Use cache but update in the background.
     new workbox.strategies.StaleWhileRevalidate({
-      // Use a custom cache name.
       cacheName: 'css-cache',
     })
   );
